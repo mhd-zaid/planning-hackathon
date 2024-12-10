078ea26887eb7b4ac27d3389e5ac97a319f3fe23
jugo92 <jugzek92@outlook.fr>
2024-12-10 23:05:58 +0100

    connect user via mail

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -3,5 +3,6 @@ import userController from "../controllers/userController.js";
 
 export default function (router) {
   router.get('/', userController.getAll);
+  router.get('/:email', userController.getUserByEmail);
   return router;
 }
