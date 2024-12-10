2a7e482c407e9dc6933e097e2236b17710758c24
jugo92 <jugzek92@outlook.fr>
2024-12-10 23:23:41 +0100

    add fixture room

--- server/src/config/router.js
+++ server/src/config/router.js
@@ -1,5 +1,7 @@
 import userRoutes from '../routes/user.js';
+import roomRoutes from '../routes/room.js';
 
 export default (app, express) => {  
   app.use('/api/users', userRoutes(express.Router()));
+  app.use('/api/rooms', roomRoutes(express.Router()));
 };
