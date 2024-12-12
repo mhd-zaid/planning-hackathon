8baed4556251bb5c217b94d3a49119fdb3e8339a
jugo92 <jugzek92@outlook.fr>
2024-12-12 20:45:23 +0100

    model replacement

---
+++ server/src/models/Replacement.js
@@ -0,0 +1,49 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default function (connection) {
+  class Replacement extends Model {
+    static associate(db) {
+        db.Replacement.belongsTo(db.User, {
+            foreignKey: 'teacherId',
+            as: 'teacher',
+        });
+        db.Replacement.belongsTo(db.Unavailability, {
+            foreignKey: 'unavailabilityId',
+            as: 'unavailability',
+        });
+    }
+  }
+
+  Replacement.init(
+    {
+        id: { type: DataTypes.UUID, primaryKey: true },
+        beginDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            unique: false,
+            validate: {
+            notEmpty: {
+                msg: 'La date de début ne peut pas être vide.',
+            },
+            },
+        },
+        endDate: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          unique: false,
+          validate: {
+            notEmpty: {
+              msg: 'La date de fin ne peut pas être vide.',
+            },
+          },
+        }
+        
+    },
+    {
+      sequelize: connection,
+      tableName: 'Replacement',
+    },
+  );
+
+  return Replacement;
+}
