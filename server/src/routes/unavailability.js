2dfc781da4b54ad288fa60cdadba3fdc40e19e85
NiruAARep <osrep6880@gmail.com>
2024-12-12 16:51:16 +0100

    Update unavailability controller and routes; remove unused methods

--- server/src/routes/unavailability.js
+++ server/src/routes/unavailability.js
@@ -2,8 +2,5 @@ import unavailabilityController from "../controllers/unavailabilityController.js
 
 export default function (router) {
   router.get("/:userId", unavailabilityController.getAll);
-  router.post("/:userId", unavailabilityController.createAbsence);
-  router.put("/:id", unavailabilityController.update);
-  router.delete("/:id", unavailabilityController.deleteUnavailability);
   return router;
 }
