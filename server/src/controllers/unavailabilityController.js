6ab9096c2a89bef5953d248e363f29dfacf5fdd1
NiruAARep <osrep6880@gmail.com>
2024-12-12 12:24:00 +0100

    Fixed typo

--- server/src/controllers/unavailabilityController.js
+++ server/src/controllers/unavailabilityController.js
@@ -1,76 +1,76 @@
 import db from "../models/index.js";
-import { uuidv4 } from "uuidv7";
+import {uuidv4} from "uuidv7";
 
 const getAll = async (req, res) => {
-  try {
-    const unavailabilities = await db.Unavailability.findAll({
-      where: {
-        userId: req.params.userId,
-      },
-    });
+    try {
+        const unavailabilities = await db.Unavailability.findAll({
+            where: {
+                userId: req.params.userId,
+            },
+        });
 
-    const userExist = await db.User.findOne({
-      where: {
-        id: req.params.userId,
-      },
-    });
+        const userExist = await db.User.findOne({
+            where: {
+                id: req.params.userId,
+            },
+        });
 
-    if (!userExist) {
-      return res.status(404).json({ message: "L'utilisateur n'existe pas" });
+        if (!userExist) {
+            return res.status(404).json({message: "L'utilisateur n'existe pas"});
+        }
+
+        return res.status(200).json(unavailabilities);
+    } catch (error) {
+        console.error("Une erreur s'est produite :", error);
+        return res.status(500).json({error: "Une erreur s'est produite"});
     }
-
-    return res.status(200).json(unavailabilities);
-  } catch (error) {
-    console.error("Une erreur s'est produite :", error);
-    return res.status(500).json({ error: "Une erreur s'est produite" });
-  }
 };
 
 const createAbsence = async (req, res) => {
-  try {
-    const userExist = await db.User.findOne({
-      where: {
-        id: req.params.userId,
-      },
-    });
+    try {
+        const userExist = await db.User.findOne({
+            where: {
+                id: req.params.userId,
+            },
+        });
 
-    if (!userExist) {
-      return res.status(404).json({ message: "L'utilisateur n'existe pas" });
+        if (!userExist) {
+            return res.status(404).json({message: "L'utilisateur n'existe pas"});
+        }
+
+        const subjectClassExist = await db.SubjectClass.findOne({
+            where: {
+                id: req.body.subjectClassId,
+            },
+        });
+
+        if (!subjectClassExist) {
+            return res.status(404).json({message: "La classe n'existe pas"});
+        }
+
+        const absence = await db.Unavailability.create({
+            id: uuidv4(),
+            date: req.body.date,
+            userId: req.params.userId,
+            subjectClassId: req.body.subjectClassId,
+        });
+
+        await db.WorkHour.destroy({
+            where: {
+                subjectClassId: subjectClassExist.id,
+            },
+        });
+        return res.status(201).json(absence);
+    } catch (error) {
+        console.error("Une erreur s'est produite :", error);
+        return res.status(500).json({error: "Une erreur s'est produite"});
     }
-
-    const subjectClassExist = await db.SubjectClass.findOne({
-      where: {
-        id: req.body.subjectClassId,
-      },
-    });
-
-    if (!subjectClassExist) {
-      return res.status(404).json({ message: "La classe n'existe pas" });
-    }
-
-    const absence = await db.Unavailability.create({
-      id: uuidv4(),
-      date: req.body.date,
-      userId: req.params.userId,
-      subjectClassId: req.body.subjectClassId,
-    });
-
-    await db.WorkHours.destroy({
-      where: {
-        userId: req.params.userId,
-        date: req.body.date,
-      },
-    });
-
-    return res.status(201).json(absence);
-  } catch (error) {
-    console.error("Une erreur s'est produite :", error);
-    return res.status(500).json({ error: "Une erreur s'est produite" });
-  }
 };
 
-const update = async (req, res) => {};
+const update = async (req, res) => {
+};
 
-const deleteUnavailability = async (req, res) => {};
+const deleteUnavailability = async (req, res) => {
+};
 
-export default { createAbsence, update, deleteUnavailability, getAll };
+export default {createAbsence, update, deleteUnavailability, getAll};
